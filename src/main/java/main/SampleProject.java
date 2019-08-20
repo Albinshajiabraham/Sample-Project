9a75ebe04859ef0d87fe0960460e5af72583b1ce
Albin <Albin@DESKTOP-8QVO4DF>
2019-08-20 15:43:07 +0530

    java

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -4,8 +4,7 @@ public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "";
-		c="Hello World!";
+		String c = "Hello World!";
 		return c;
 	}
 	public static int add(int a,int b) {
